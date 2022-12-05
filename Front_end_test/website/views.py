caf081d740ba570546aeba5da23c41cf88a2487c
Matthew <90485588+MJDoss@users.noreply.github.com>
2022-12-05 15:29:36 -0600

    Minor fixes

--- Front_end_test/website/views.py
+++ Front_end_test/website/views.py
@@ -6,3 +6,9 @@ views = Blueprint('views', __name__)
 @views.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("index.html")
+
+
+
+@views.route('/accountMade', methods=['GET'])
+def accountMade():
+    return render_template("index.html")
